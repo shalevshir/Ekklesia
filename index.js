1441011b928847af38bf572182994bc5ec2e2094
shalevshir <shalevshir@gmail.com>
2024-02-05 17:17:48 +0200

    specify cors origins

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ connectDB();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors())
+app.use(cors({origin: ['http://localhost']}))
 app.use(express.json());
 app.use(express.static('public'));
 app.get('/', (req, res) => {
