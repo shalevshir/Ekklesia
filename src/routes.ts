70757ca35c35d0b4b2e1d6e26683d491cb5eca6a
shalevshir <shalevshir@gmail.com>
2024-02-11 00:04:20 +0200

    revert test

--- src/routes.ts
+++ src/routes.ts
@@ -65,7 +65,6 @@ const updateSessionsInCommittees = async (req: Request, res: Response) => {
 };
 
 const fetchQueries = async (req: Request, res: Response) => {
-  console.log(process.env.REDISCLOUD_URL, 'process.env.REDISCLOUD_URL')
   await workerQueue.add('fetchQueries');
   res.send("Job sent to worker queue.");
 };
