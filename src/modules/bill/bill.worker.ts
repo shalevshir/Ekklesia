4fcab8db4ed4dcd5ac6e2b9258bc82dcc6ba0cfd
shalevshir <shalevshir@gmail.com>
2024-02-21 20:38:36 +0200

    update bills category by committee

---
+++ src/modules/bill/bill.worker.ts
@@ -0,0 +1,31 @@
+import logger from "../../utils/logger";
+import committeeRepo from "../committee/committee.repo";
+import billRepo from "./bill.repo";
+
+
+class billWorker {
+    async updateBillsMainCategory(job:any){
+        try {
+            logger.info({message:"Update bills main category process started", jobId: job.id});
+            const bills = await billRepo.find({});
+            for(const bill of bills){
+                if(!bill.committee){
+                    continue;
+                }
+                const category = committeeRepo.categoryByCommitteeId(bill.committee.toString());
+                if(!category){
+                    continue;
+                }
+                await billRepo.update({_id: bill._id}, {categories: [category]});
+            }
+            // await billRepo.updatebillsMainCategory();
+            logger.info("Update bills main category process finished");
+            return true;
+        } catch (error) {
+            logger.error("Error in updatebillsMainCategory", error);
+            throw error;
+        }
+    }
+}
+
+export default new billWorker();
--- src/modules/committee/committee.repo.ts
+++ src/modules/committee/committee.repo.ts
@@ -16,6 +16,33 @@ class CommitteeRepo extends BaseRepo<Committee> {
     993: "investigation",
   };
 
+  committeeIdToCategoryMap: Record<string,string> = {
+     "65bbe7d1e8350e32474895d8":"65d4797a76e4a97122327c9c",
+    "65bbe7d1e8350e32474895f8":"65be68105867b500fd71e7dd",
+    "65bbe7d0e8350e32474895b0":"65be68105867b500fd71e7d4",
+    "65bbe7d0e8350e32474895ac":"65be68105867b500fd71e7d0",
+    "65bbe7d0e8350e32474895be":"65be68105867b500fd71e7db",
+    "65bbe7d1e8350e32474895ee":"65be68105867b500fd71e7d1",
+    "65bbe7d1e8350e32474895dc":"",//הוועדה למיזמים ציבורייםת
+    "65bbe7d1e8350e32474895f4":"65be68105867b500fd71e7d4",
+    "65bbe7d1e8350e32474895f2":"65be68105867b500fd71e7d1",
+    "65bbe7d0e8350e32474895ca":"65be68105867b500fd71e7d1",
+    "65bbe7d0e8350e32474895b2":"65be68105867b500fd71e7d6",
+    "65bbe7d0e8350e32474895d0":"",//"הוועדה המיוחדת לזכויות הילד"
+    "65bbe7d0e8350e32474895b6":"",//"ועדת הכנסת"
+    "65bbe7d0e8350e32474895ba":"65be68105867b500fd71e7d9",
+    "65bbe7d0e8350e32474895c2":"65be68105867b500fd71e7dd",
+    "65bbe7d1e8350e32474895fa":"65be68105867b500fd71e7dd",
+    "65bbe7d0e8350e32474895b4":"65be68105867b500fd71e7d7",
+    "65bbe7d0e8350e32474895cc":"65be68105867b500fd71e7d2",
+    "65bbe7d1e8350e32474895f0":"",//"הוועדה המיוחדת לתיקונים לחוק יסוד: הממשלה"
+    "65bbe7d1e8350e32474895e6":"65be68105867b500fd71e7d8",
+    "65bbe7d0e8350e32474895ae":"65be68105867b500fd71e7d3",
+    "65bbe7d0e8350e32474895aa":"65be68105867b500fd71e7d1",
+    "65bbe7d0e8350e32474895c8":"",//הוועדה המיוחדת לדיון בהצעת חוק-יסוד: הממשלה (תיקון - שר נוסף במשרד) (פ/81/25) ובהצעת חוק-יסוד: הממשלה (תיקון – כשירותם של שרים) (פ/91/25)
+    "65bbe7d0e8350e32474895c0":"65be68105867b500fd71e7dc",
+    "65bbe7d1e8350e32474895f6":"65be68105867b500fd71e7d4"
+  }
   constructor() {
     super(CommitteeModel);
   }
@@ -71,6 +98,10 @@ class CommitteeRepo extends BaseRepo<Committee> {
         }
       );
     }
+  }
+
+  categoryByCommitteeId(id: string) {
+    return this.committeeIdToCategoryMap[id];
   }
 }
 
