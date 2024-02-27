78481f17ead57d242a9d27ee5c3e0228050d6e2c
shalevshir <shalevshir@gmail.com>
2024-02-27 11:20:17 +0200

    add connection export

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,3 +1,4 @@
+import { Connection } from 'mongoose';
 import { mongoose } from "@typegoose/typegoose";
 
 const dbName = process.env.DB_NAME
@@ -5,11 +6,12 @@ const dbPass = process.env.DB_PASSWORD;
 const connectionUri = process.env.NODE_ENV === "production" ?
    `mongodb+srv://${dbName}:${dbPass}@ekklesia.xyo4j4r.mongodb.net/?retryWrites=true&w=majority`:
   `mongodb://localhost:27018`
-  
+export let connection: Connection;
 const connectDB = async () => {
   try {
-    await mongoose.connect(connectionUri);
+    const connectionObj = await mongoose.connect(connectionUri,{connectTimeoutMS: 3000, socketTimeoutMS: 3000, waitQueueTimeoutMS: 3000});
     console.log("MongoDB Connected...");
+    connection = connectionObj.connections[0]
   } catch (err: any) {
     console.error(err.message);
     process.exit(1);
