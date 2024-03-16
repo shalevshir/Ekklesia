53d139330194c9a7457d7e3c7b2c57aa9ed00b6c
shalevshir <shalevshir@gmail.com>
2024-03-16 13:39:49 +0200

    lint file

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,21 +1,23 @@
 import { Connection } from 'mongoose';
-import { mongoose } from "@typegoose/typegoose";
+import { mongoose } from '@typegoose/typegoose';
 
-const dbName = process.env.DB_NAME
+const dbName = process.env.DB_NAME;
 const dbPass = process.env.DB_PASSWORD;
-const connectionUri = process.env.NODE_ENV === "production" ?
-   `mongodb+srv://${dbName}:${dbPass}@ekklesia.xyo4j4r.mongodb.net/?retryWrites=true&w=majority`:
-  `mongodb://localhost:27018`
+const connectionUri = process.env.NODE_ENV === 'production' ?
+   `mongodb+srv://${ dbName }:${ dbPass }@ekklesia.xyo4j4r.mongodb.net/?retryWrites=true&w=majority` :
+  'mongodb://localhost:27018';
 export let connection: Connection;
 const connectDB = async () => {
   try {
-    const connectionObj = await mongoose.connect(connectionUri,{connectTimeoutMS: 3000, socketTimeoutMS: 3000, waitQueueTimeoutMS: 3000});
-    console.log("MongoDB Connected...");
-    connection = connectionObj.connections[0]
+    const connectionObj = await mongoose.connect(
+      connectionUri, { connectTimeoutMS: 3000, socketTimeoutMS: 3000, waitQueueTimeoutMS: 10000 }
+    );
+    console.log('MongoDB Connected...');
+    connection = connectionObj.connections[0];
   } catch (err: any) {
     console.error(err.message);
     process.exit(1);
   }
 };
 
-export { connectDB }
+export { connectDB };
