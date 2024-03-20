18f2ff37c7ffdc154b377acf2878b5ecfc58fef7
shalevshir <shalevshir@gmail.com>
2024-03-20 08:45:49 +0200

    lint knesset service

--- src/utils/knesset-api.service.ts
+++ src/utils/knesset-api.service.ts
@@ -1,75 +1,72 @@
-import axios from "axios";
-import _, { filter } from "lodash";
-import { mapIdToRole } from "../types/roles.enum";
-import { Person } from "../modules/person/person.model";
-import logger from "./logger";
+import axios from 'axios';
+import _ from 'lodash';
+import { mapIdToRole } from '../types/roles.enum';
+import logger from './logger';
 
 function wait(seconds: number) {
   return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 }
 
 class KnessetService {
-  baseKnessetUrl = "http://knesset.gov.il/Odata/";
-  baseKnessetUrlV4 = "http://knesset.gov.il/OdataV4/";
+  baseKnessetUrl = 'http://knesset.gov.il/Odata/';
+  baseKnessetUrlV4 = 'http://knesset.gov.il/OdataV4/';
   dataBases = {
-    parliament: "ParliamentInfo.svc",
-    votes: "Votes.svc",
-    lobby: "Lobbyist.svc",
-    mmm: "MMM.svc",
+    parliament: 'ParliamentInfo.svc',
+    votes: 'Votes.svc',
+    lobby: 'Lobbyist.svc',
+    mmm: 'MMM.svc'
   };
   databaseV4 = {
-    parliament: "ParliamentInfo",
-    votes: "Votes",
-    lobby: "Lobbyist",
-    mmm: "MMM",
+    parliament: 'ParliamentInfo',
+    votes: 'Votes',
+    lobby: 'Lobbyist',
+    mmm: 'MMM'
   };
 
   axiosInstance = axios.create({
-    baseURL: this.baseKnessetUrl,
+    baseURL: this.baseKnessetUrl
   });
 
   axiosInstanceV4 = axios.create({
-    baseURL: this.baseKnessetUrlV4,
+    baseURL: this.baseKnessetUrlV4
   });
 
-  async getMks(): Promise<any[] | undefined>  {
-    try{
+  async getMks(): Promise<any[] | undefined> {
+    try {
       const { data } = await this.axiosInstance.get(
-        `${this.baseKnessetUrlV4}ParliamentInfo/KNS_PersonToPosition?$filter=KnessetNum eq 25&$expand=KNS_Person`
+        `${ this.baseKnessetUrlV4 }ParliamentInfo/KNS_PersonToPosition?$filter=KnessetNum eq 25&$expand=KNS_Person`
       );
-      if(!data.value){
-        throw new Error("No persons found");
+      if (!data.value) {
+        throw new Error('No persons found');
       }
       const persons = new Set();
-      for(const position of data.value) {
+      for (const position of data.value) {
         const personObj = position.KNS_Person;
-        //find person on the set
-        const existPerson = Array.from(persons).find((person: any) => person.Id === personObj.Id) as any;  
-        if(existPerson) {
+        // find person on the set
+        const existPerson = Array.from(persons).find((person: any) => person.Id === personObj.Id) as any;
+        if (existPerson) {
           existPerson.positions.push(position);
         } else {
-          personObj.positions = [position];
+          personObj.positions = [ position ];
           persons.add(personObj);
         }
-
       }
       return Array.from(persons);
-    } catch(error) {
+    } catch (error) {
       throw error;
     }
-    
   }
 
   async accumulateData(data: any) {
     const dataArray = data.value ? data.value : data;
-    while (data["odata.nextLink"]) {
+    while (data['odata.nextLink']) {
       const { data: nextData } = await this.axiosInstance.get(
-        `${this.dataBases.parliament}/${data["odata.nextLink"]}`
+        `${ this.dataBases.parliament }/${ data['odata.nextLink'] }`
       );
       data = nextData;
       const toPush = data.value ? data.value : data;
       dataArray.push(...toPush);
-      await wait(1)
+      await wait(1);
     }
     return dataArray;
   }
@@ -80,29 +77,28 @@ class KnessetService {
       firstNameHeb: mk.FirstName,
       lastNameHeb: mk.LastName,
       email: mk.Email,
-      gender: mk.GenderDesc === "זכר" ? "male" : "female",
+      gender: mk.GenderDesc === 'זכר' ? 'male' : 'female',
       faction: mk.faction,
       roles: this.mapRoles(mk.roles),
       committees: mk.committees,
-      minister: mk.minister,
+      minister: mk.minister
     }));
   }
 
-  mapRoles(kns_positions: any) {
-    const roles = [...kns_positions].map((roleId) => ({
+  mapRoles(knsPositions: any) {
+    const roles = [ ...knsPositions ].map((roleId) => ({
       title: mapIdToRole[roleId],
-      isCurrent: true,
+      isCurrent: true
     }));
     return roles;
   }
 
   async getMainCommittees() {
     try {
-      
       const { data } = await this.axiosInstance.get(
-        `${this.dataBases.parliament}/KNS_Committee()?$filter=KnessetNum eq 25 and ParentCommitteeID eq null`
+        `${ this.dataBases.parliament }/KNS_Committee()?$filter=KnessetNum eq 25 and ParentCommitteeID eq null`
       );
-        
+
       const committees = await this.accumulateData(data);
 
       return committees;
@@ -114,11 +110,10 @@ class KnessetService {
 
   async getSubCommittees() {
     try {
-      
       const { data } = await this.axiosInstance.get(
-        `${this.dataBases.parliament}/KNS_Committee()?$filter=KnessetNum eq 25`
+        `${ this.dataBases.parliament }/KNS_Committee()?$filter=KnessetNum eq 25`
       );
-        
+
       const committees = await this.accumulateData(data);
 
       return committees;
@@ -130,7 +125,7 @@ class KnessetService {
   async getCommitteeSessions(committeeId: number) {
     try {
       const { data } = await this.axiosInstance.get(
-        `${this.dataBases.parliament}/KNS_Committee(${committeeId})/KNS_CommitteeSessions`
+        `${ this.dataBases.parliament }/KNS_Committee(${ committeeId })/KNS_CommitteeSessions`
       );
       return this.accumulateData(data);
     } catch (error) {
@@ -139,9 +134,11 @@ class KnessetService {
   }
 
   async getCommitteeSessionTranscript(committeeSessionId: number) {
+    const url = `${ this.dataBases.parliament }` +
+      `/KNS_CommitteeSession(${ committeeSessionId })/KNS_DocumentCommitteeSessions/?$filter=GroupTypeID eq 23`;
     try {
       const { data } = await this.axiosInstance.get(
-        `${this.dataBases.parliament}/KNS_CommitteeSession(${committeeSessionId})/KNS_DocumentCommitteeSessions/?$filter=GroupTypeID eq 23`
+        url
       );
       return data.value;
     } catch (error) {
@@ -152,7 +149,7 @@ class KnessetService {
   async getQueries() {
     try {
       const { data } = await this.axiosInstanceV4.get(
-        `${this.databaseV4.parliament}/KNS_Query?$filter=KnessetNum eq 25&$expand=KNS_GovMinistry`
+        `${ this.databaseV4.parliament }/KNS_Query?$filter=KnessetNum eq 25&$expand=KNS_GovMinistry`
       );
       return data.value;
     } catch (error) {
@@ -163,7 +160,7 @@ class KnessetService {
   async getQueriesDocuments(queryId: number) {
     try {
       const { data } = await this.axiosInstanceV4.get(
-        `${this.databaseV4.parliament}/KNS_DocumentQuery?$filter=QueryID eq ${queryId}`
+        `${ this.databaseV4.parliament }/KNS_DocumentQuery?$filter=QueryID eq ${ queryId }`
       );
       return this.accumulateData(data);
     } catch (error) {
@@ -173,7 +170,7 @@ class KnessetService {
   async getBills() {
     try {
       const { data } = await this.axiosInstance.get(
-        `${this.dataBases.parliament}/KNS_Bill?$filter=KnessetNum eq 25&$expand=KNS_BillInitiators`
+        `${ this.dataBases.parliament }/KNS_Bill?$filter=KnessetNum eq 25&$expand=KNS_BillInitiators`
       );
       return this.accumulateData(data);
     } catch (error) {
@@ -183,33 +180,33 @@ class KnessetService {
 
   async getBillsLinks(billsIds: number[]) {
     const updateData = [];
-    try{
-      logger.info(`Getting bills links for ${billsIds.length} bills`);
+    try {
+      logger.info(`Getting bills links for ${ billsIds.length } bills`);
       let billNumber = 1;
       for await (const billId of billsIds) {
-        logger.info(`Getting bill #${billNumber} out of ${billsIds.length}`, billId)
+        logger.info(`Getting bill #${ billNumber } out of ${ billsIds.length }`, billId);
         const { data } = await this.axiosInstance.get(
-          `${this.dataBases.parliament}/KNS_DocumentBill?$filter=BillID eq ${billId}&$orderby=LastUpdatedDate desc`
+          `${ this.dataBases.parliament }/KNS_DocumentBill?$filter=BillID eq ${ billId }&$orderby=LastUpdatedDate desc`
         );
-        const getLatest = (data: any): any =>{
-          const first = _.first(data) as any
-          if(first.GroupTypeID !==  17 || first.GroupTypeID) return first
-          
-          //remove government decisions documents
-          return getLatest(data.slice(1))
-        }
-        
-        const latest = getLatest(data.value)
+        const getLatest = (data: any): any => {
+          const first = _.first(data) as any;
+          if (first.GroupTypeID !== 17 || first.GroupTypeID) return first;
+
+          // remove government decisions documents
+          return getLatest(data.slice(1));
+        };
+
+        const latest = getLatest(data.value);
         updateData.push({
           originId: billId,
           billLink: latest.FilePath
         });
-        logger.info(`Got bill #${billNumber++} out of ${billsIds.length}`, billId)
+        logger.info(`Got bill #${ billNumber++ } out of ${ billsIds.length }`, billId);
         await wait(0.3);
       }
       return updateData;
-    } catch(error) {
-      logger.error("Error getting bills links", error);
+    } catch (error) {
+      logger.error('Error getting bills links', error);
       throw error;
     }
   }
