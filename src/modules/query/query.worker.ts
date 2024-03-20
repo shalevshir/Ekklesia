1becb36698c11bd9354b21efb1ed85e9673e56cc
shalevshir <shalevshir@gmail.com>
2024-03-20 08:54:36 +0200

    lint query worker

--- src/modules/query/query.worker.ts
+++ src/modules/query/query.worker.ts
@@ -1,32 +1,32 @@
-import queryRepo from "./query.repo";
-import knessetApiService from "../../utils/knesset-api.service";
-import logger from "../../utils/logger";
+import queryRepo from './query.repo';
+import logger from '../../utils/logger';
 
 
 class queriesWorker {
-    async fetchQueries(job:any){
-        try {
-            logger.info({message:"Fetch queries process started", jobId: job.id});
-            await queryRepo.fetchQueriesFromKnesset();
-            logger.info("Fetching queries process finished");
-            return true;
-        } catch (error) {
-            logger.error("Error in fetchQueries", error);
-            throw error;
-        }
+  async fetchQueries(job: any, done: Function) {
+    try {
+      logger.info({ message: 'Fetch queries process started', jobId: job.id });
+      done();
+      await queryRepo.fetchQueriesFromKnesset();
+      logger.info('Fetching queries process finished');
+      return true;
+    } catch (error) {
+      logger.error('Error in fetchQueries', error);
+      throw error;
     }
+  }
 
-    async updateCategoriesByMinistry(job:any){
-        try {
-            logger.info({message:"Update categories by ministry process started", jobId: job.id});
-            await queryRepo.updateCategoriesByMinistry();
-            logger.info("Update categories by ministry process finished");
-            return true;
-        } catch (error) {
-            logger.error("Error in update categories by ministry", error);
-            throw error;
-        }
-    };
+  async updateCategoriesByMinistry(job: any) {
+    try {
+      logger.info({ message: 'Update categories by ministry process started', jobId: job.id });
+      await queryRepo.updateCategoriesByMinistry();
+      logger.info('Update categories by ministry process finished');
+      return true;
+    } catch (error) {
+      logger.error('Error in update categories by ministry', error);
+      throw error;
+    }
+  };
 }
 
 export default new queriesWorker();
