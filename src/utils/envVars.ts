39d8b8b890556df0245a89122f64ee7a85926676
shalevshir <shalevshir@gmail.com>
2024-03-22 10:14:47 +0200

    create env vars util

---
+++ src/utils/envVars.ts
@@ -0,0 +1,52 @@
+/* eslint-disable no-process-env */
+// Import necessary modules
+import logger from './logger';
+
+// Enum for environment variables
+enum EnvVarEnum {
+  NODE_ENV = 'NODE_ENV',
+  PORT = 'PORT',
+  REDISCLOUD_URL = 'REDISCLOUD_URL',
+  DB_NAME = 'DB_NAME',
+  DB_PASSWORD = 'DB_PASSWORD',
+  HUGGINGFACEHUB_API_KEY = 'HUGGINGFACEHUB_API_KEY'
+}
+
+// Load environment variables in non-production environments
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+// Prepare a list of environment variable keys
+const envVarList: EnvVarEnum[] = Object.values(EnvVarEnum);
+
+// Initialize a Map to hold the environment variables
+const envVars = new Map<EnvVarEnum, string>();
+
+// Populate the Map with environment variables, log if any are missing
+envVarList.forEach((envVar) => {
+  if (!process.env[envVar]) {
+    logger.error('Missing env var: ' + envVar);
+  } else {
+    envVars.set(envVar, process.env[envVar] as string);
+  }
+});
+
+type EnvVars = {
+  [key in EnvVarEnum]?: string;
+};
+
+// Create a Proxy for more convenient access to the environment variables
+const envProxy: EnvVars = new Proxy({}, {
+  get(target, name: string) {
+    const envVar = envVars.get(name as EnvVarEnum);
+    if (!envVar) {
+      logger.error(`Missing env var: ${ name }`);
+      return;
+    }
+    return envVar;
+  }
+});
+
+// Export the Proxy and the EnvVarEnum for use elsewhere in the application
+export { envProxy as envVars };
