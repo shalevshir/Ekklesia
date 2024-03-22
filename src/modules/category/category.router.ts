9cb6ed995d7eea9c146fbe79072f9b5fcd50e224
shalevshir <shalevshir@gmail.com>
2024-03-22 10:16:56 +0200

    lint file

--- src/modules/category/category.router.ts
+++ src/modules/category/category.router.ts
@@ -2,47 +2,43 @@ import express, { Request, Response } from 'express';
 import categoriesRepo from './category.repo';
 import { handleError } from '../../utils/errors.utils';
 import queryRepo from '../query/query.repo';
-import gptService from '../../utils/gpt.service';
-import { Query } from '../query/query.model';
-import { DocumentType } from '@typegoose/typegoose';
 import { getFileAsText } from '../../utils/files.service';
 // Create a new router instance
 const categoryRouter = express.Router();
 
 categoryRouter.get('/', async (req: Request, res: Response) => {
-    try {
-        const categories = await categoriesRepo.getAllCategories();
-        res.send(categories);
-    } catch (error) {
-        handleError("error in get categories", error, res);
-    }
+  try {
+    const categories = await categoriesRepo.getAllCategories();
+    res.send(categories);
+  } catch (error) {
+    handleError('error in get categories', error, res);
+  }
 });
 
 categoryRouter.get('/subCategories/:categoryName', async (req: Request, res: Response) => {
-    try {
-        const subCategories = await categoriesRepo.getSubCategories(req.params.categoryName);
-        res.send(subCategories);
-    }
-    catch (error) {
-        handleError("error in get sub categories", error, res);
-    }
+  try {
+    const subCategories = await categoriesRepo.getSubCategories(req.params.categoryName);
+    res.send(subCategories);
+  } catch (error) {
+    handleError('error in get sub categories', error, res);
+  }
 });
 
 categoryRouter.get('/query/:id', async (req: Request, res: Response) => {
-    try {
-        const queryId = req.params.id;
-        const query = await queryRepo.findOne({_id:queryId})
-        if(!query || !query.queryLink){
-            throw new Error(`Query ${queryId} not found`);
-        }
-        const queryText = await getFileAsText(query.queryLink)
-
-        const categorization = await gptService.categorizeQuery(queryText);
-        res.json(categorization).send();
-    } catch (error) {
-        handleError("error in get categories tree", error, res);
+  try {
+    const queryId = req.params.id;
+    const query = await queryRepo.findOne({ _id: queryId });
+    if (!query || !query.queryLink) {
+      throw new Error(`Query ${ queryId } not found`);
     }
-})
+    const queryText = await getFileAsText(query.queryLink);
+
+    // const categorization = await embeddingService.categorizeQuery(queryText);
+    // res.json(categorization).send();
+  } catch (error) {
+    handleError('error in get categories tree', error, res);
+  }
+});
 
 // Export the router
 export default categoryRouter;
