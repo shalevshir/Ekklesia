e99ef62d30a6930851d94910c61b96a719f90833
shalevshir <shalevshir@gmail.com>
2024-03-22 16:04:37 +0200

    organize jobs

--- src/worker.ts
+++ src/worker.ts
@@ -6,20 +6,29 @@ import billWorker from './modules/bill/bill.worker';
 import committeeSessionWorker from './modules/committeeSession/committeeSession.worker';
 import { envVars } from './utils/envVars';
 import personWorker from './modules/person/person.worker';
+import committeeWorker from './modules/committee/committee.worker';
 
 const startWorker = async () => {
   await connectDB();
 
   const workerQueue = new Queue('workerQueue', envVars.REDISCLOUD_URL || 'redis://127.0.0.1:6379');
-
+  // Person
   workerQueue.process('fetchPeople', personWorker.fetchPeople);
 
+  // Committee
+  workerQueue.process('fetchCommitteesFromKnessetApi', committeeWorker.fetchCommitteesFromKnessetApi);
+  workerQueue.process('updateCommitteesMembers', committeeWorker.updateCommitteesMembers);
+
+  // Query
   workerQueue.process('fetchQueries', queryWorker.fetchQueries);
   workerQueue.process('updateCategoriesByMinistry', queryWorker.updateCategoriesByMinistry);
 
+  // Bill
   workerQueue.process('fetchBills', billWorker.fetchBills);
   workerQueue.process('updateBillsStages', billWorker.updateBillsStages);
   workerQueue.process('updateBillDocument', billWorker.updateBillDocument);
+
+  // CommitteeSession
   workerQueue.process('updateCommitteeSessions', committeeSessionWorker.fetchCommitteesSessions);
   logger.info('Worker is running...');
 };
