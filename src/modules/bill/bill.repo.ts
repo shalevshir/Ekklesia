e777762237e032d03a4bc29e31d8b97ec950c205
shalevshir <shalevshir@gmail.com>
2024-03-22 10:16:09 +0200

    lint bills files

--- src/modules/bill/bill.repo.ts
+++ src/modules/bill/bill.repo.ts
@@ -1,74 +1,78 @@
-import BaseRepo from "../../abstracts/repo.abstract";
-import personRepo from "../person/person.repo";
-import BillModel, {Bill, StageSchema, Vote} from "./bill.model";
-import knessetApiService from "../../utils/knesset-api.service";
-import committeeRepo from "../committee/committee.repo";
-import _ from "lodash";
-import { connection } from "../../utils/db";
-import PersonModel from "../person/person.model";
-import logger from "../../utils/logger";
+import { DocumentType } from '@typegoose/typegoose';
+import BaseRepo from '../../abstracts/repo.abstract';
+import personRepo from '../person/person.repo';
+import BillModel, { Bill, Vote } from './bill.model';
+import knessetApiService from '../../utils/knesset-api.service';
+import committeeRepo from '../committee/committee.repo';
+import _ from 'lodash';
+import { connection } from '../../utils/db';
+import PersonModel from '../person/person.model';
+import logger from '../../utils/logger';
+import { getFileAsText } from '../../utils/files.service';
+import embeddingService from '../../utils/embedding.service';
+
 class BillsRepo extends BaseRepo<Bill> {
   constructor() {
     super(BillModel);
   }
-  types: Record<number,string>  = {
-    54: "private",
-    53: "governmental",
-    55: "committee",
+  types: Record<number, string> = {
+    54: 'private',
+    53: 'governmental',
+    55: 'committee'
   };
-  statuses: Record<number,string> = {
-    101: "preparationForFirstVote",
-    104: "onTableForEarlyDiscussion",
-    106: "inKnessetCommittee",
-    108: "preparationForFirstVote",
-    109: "approvedInCommitteeForFirstVote",
-    110: "requestForContinuousVoteDeniedInPlenum",
-    111: "inPlenumForFirstVote",
-    113: "preparationForSecondAndThirdVote",
-    114: "inPlenumForSecondAndThirdVote",
-    115: "returnedToCommitteeForPreparationForThirdVote",
-    117: "inPlenumForThirdVote",
-    118: "approvedInPlenumForThirdVote",
-    120: "inPlenumForContinuousVote",
-    122: "mergedWithAnotherBill",
-    124: "movedToOrderOfTheDay",
-    126: "splitForEarlyDiscussion",
-    130: "onTableForSecondAndThirdVote",
-    131: "onTableForThirdVote",
-    140: "removedFromOrderOfTheDayByCommitteeRequest",
-    141: "onTableForFirstVote",
-    142: "inKnessetCommittee",
-    143: "removedFromOrderOfTheDayByCommitteeRequest",
-    150: "inPlenumForEarlyDiscussion",
-    158: "splitInPlenum",
-    161: "splitInPlenum",
-    162: "splitInPlenum",
-    165: "splitInPlenum",
-    167: "approvedInCommitteeForFirstVote",
-    169: "mergedInKnessetCommittee",
-    175: "inCommitteeForContinuousVote",
-    176: "requestForContinuousVoteDeniedInCommittee",
-    177: "stopped",
-    178: "approvedInCommitteeForSecondAndThirdVote",
-    179: "approvedInCommitteeForSecondAndThirdVote",
+  statuses: Record<number, string> = {
+    101: 'preparationForFirstVote',
+    104: 'onTableForEarlyDiscussion',
+    106: 'inKnessetCommittee',
+    108: 'preparationForFirstVote',
+    109: 'approvedInCommitteeForFirstVote',
+    110: 'requestForContinuousVoteDeniedInPlenum',
+    111: 'inPlenumForFirstVote',
+    113: 'preparationForSecondAndThirdVote',
+    114: 'inPlenumForSecondAndThirdVote',
+    115: 'returnedToCommitteeForPreparationForThirdVote',
+    117: 'inPlenumForThirdVote',
+    118: 'approvedInPlenumForThirdVote',
+    120: 'inPlenumForContinuousVote',
+    122: 'mergedWithAnotherBill',
+    124: 'movedToOrderOfTheDay',
+    126: 'splitForEarlyDiscussion',
+    130: 'onTableForSecondAndThirdVote',
+    131: 'onTableForThirdVote',
+    140: 'removedFromOrderOfTheDayByCommitteeRequest',
+    141: 'onTableForFirstVote',
+    142: 'inKnessetCommittee',
+    143: 'removedFromOrderOfTheDayByCommitteeRequest',
+    150: 'inPlenumForEarlyDiscussion',
+    158: 'splitInPlenum',
+    161: 'splitInPlenum',
+    162: 'splitInPlenum',
+    165: 'splitInPlenum',
+    167: 'approvedInCommitteeForFirstVote',
+    169: 'mergedInKnessetCommittee',
+    175: 'inCommitteeForContinuousVote',
+    176: 'requestForContinuousVoteDeniedInCommittee',
+    177: 'stopped',
+    178: 'approvedInCommitteeForSecondAndThirdVote',
+    179: 'approvedInCommitteeForSecondAndThirdVote'
   };
   async fetchBillsFromKnesset() {
     const billsData = await knessetApiService.getBills();
     const arrangedBills = await this.arrangeBills(billsData);
-    await this.updateMany(arrangedBills, { upsert:true});
+    await this.updateMany(arrangedBills, { upsert: true });
   }
   async arrangeBills(bills: any[]) {
-    let billsArranged: any[] = [];
+    const billsArranged: any[] = [];
     for await (const bill of bills) {
       bill.initiator = [];
       const billType: string = this.types[bill.SubTypeID];
       const billStatus: string = this.statuses[bill.StatusID];
       const committee = await committeeRepo.findOne({
-        originId: bill.CommitteeID,
+        originId: bill.CommitteeID
       });
       for await (const initiator of bill.KNS_BillInitiators) {
         const person = await personRepo.findOne({
-          originId: initiator.PersonID,
+          originId: initiator.PersonID
         });
         if (!person) continue;
 
@@ -85,7 +89,7 @@ class BillsRepo extends BaseRepo<Bill> {
         number: bill.Number,
         pNumber: bill.PrivateNumber,
         committee: committee ? committee._id : null,
-        initiators: bill.initiator,
+        initiators: bill.initiator
       };
       billsArranged.push(arrangedBill);
     }
@@ -94,20 +98,20 @@ class BillsRepo extends BaseRepo<Bill> {
 
   async updateBillsFromKnesset() {
     const bills = await this.find({});
-    const billsIds:number[] = bills.map((bill: any) => bill.originId) as number[];
+    const billsIds: number[] = bills.map((bill: any) => bill.originId) as number[];
     const billsData = await knessetApiService.getBillsLinks(billsIds);
-    if(!billsData) {
-      throw new Error("No bills found");
+    if (!billsData) {
+      throw new Error('No bills found');
     }
     await this.updateMany(billsData);
   }
 
   async updateBillsDocumentsLinks() {
     const bills = await this.model.find({ categories: { $ne: null } }).lean();
-    const billsIds:number[] = bills.map((bill: any) => bill.originId) as number[];
+    const billsIds: number[] = bills.map((bill: any) => bill.originId) as number[];
     const billsData = await knessetApiService.getBillsLinks(billsIds);
-    if(!billsData) {
-      throw new Error("No bills found");
+    if (!billsData) {
+      throw new Error('No bills found');
     }
     await this.updateMany(billsData);
   }
@@ -115,52 +119,77 @@ class BillsRepo extends BaseRepo<Bill> {
   async updateBillsStages() {
     try {
       const rawDataCollection = connection.collection('rawVotesData');
-      //Filter by status when we know relevant statuses
+      // Filter by status when we know relevant statuses
       const bills = await this.model.find({ categories: { $ne: null } }).lean() as any[];
-      logger.info({message:`Updating stages for ${bills.length} bills `});
+      logger.info({ message: `Updating stages for ${ bills.length } bills ` });
       let billNumber = 1;
       for (const bill of bills) {
-        logger.info({message:`updating bill #${billNumber} out of ${bills.length}`, billId: bill._id});
+        logger.info({ message: `updating bill #${ billNumber } out of ${ bills.length }`, billId: bill._id });
         const billData = await rawDataCollection.find({ 'Item ID': +bill.originId }).toArray();
-
-        if (!billData.length){
-          logger.info({message:`No stages found for bill #${billNumber} out of ${bills.length}`, billId: bill._id});
+        if (!billData.length) {
+          logger.info({ message: `No stages found for bill #${ billNumber } out of ${ bills.length }`, billId: bill._id });
           billNumber++;
           continue;
         }
-  
+
         const stagesGroupedBySession = _.groupBy(billData, 'Vote ID');
-        const personIds = new Set(billData.filter(data => data['MK ID']).map(data => data['MK ID']));
+        const personIds = new Set(billData.filter((data) => data['MK ID']).map((data) => data['MK ID']));
         const personIdToDbIdMap = new Map();
-  
-        await Promise.all([...personIds].map(id => 
-          PersonModel.findOne({ originId: +id }).then(person => person && personIdToDbIdMap.set(id, person._id))
+
+        await Promise.all([ ...personIds ].map((id) =>
+          PersonModel.findOne({ originId: +id }).then((person) => person && personIdToDbIdMap.set(id, person._id))
         ));
-  
-        const mappedStages = Object.entries(stagesGroupedBySession).map(([stageId, votes]) => {
-          if(!votes.length) return;
-          const filteredVotes = votes.filter(vote => vote['MK ID'] && personIdToDbIdMap.has(vote['MK ID'])).map(vote => ({
+
+        const mappedStages = Object.entries(stagesGroupedBySession).map(([ stageId, votes ]) => {
+          if (!votes.length) return;
+          const filteredVotes = votes.filter((vote) => vote['MK ID'] && personIdToDbIdMap.has(vote['MK ID'])).map((vote) => ({
             person: personIdToDbIdMap.get(vote['MK ID']),
             vote: vote['הצבעה'] === 'בעד' ? Vote.FOR : Vote.AGAINST,
             originId: vote['Vote ID'],
-            sessionId: vote['Session ID'],
+            sessionId: vote['Session ID']
           }));
-  
+
           return {
             date: new Date(votes[0]['תאריך']),
-            votes: filteredVotes,
+            votes: filteredVotes
           };
-        }).filter(stage => !!stage);
-  
+        }).filter((stage) => !!stage);
+
         await this.model.findOneAndUpdate({ _id: bill._id }, { stages: mappedStages });
         billNumber++;
-        logger.info({message:`Update stages for bill #${billNumber} completed`, billId: bill._id});
+        logger.info({ message: `Update stages for bill #${ billNumber } completed`, billId: bill._id });
       }
     } catch (error) {
-      logger.error({message:"Error in updateBillStages", error});
+      logger.error({ message: 'Error in updateBillStages', error });
       throw error;
     }
-  }  
+  }
+
+  async embedBills() {
+    const bills = await this.model.find<DocumentType<Bill>>({ categories: { $ne: null } }).lean();
+    logger.info({ message: `Embedding ${ bills.length } bills ` });
+    let billNumber = 1;
+    const toPromise = [];
+    for (const bill of bills) {
+      logger.info({ message: `Embedding bill #${ billNumber } out of ${ bills.length }`, billId: bill._id });
+      let query = '';
+      if (bill.billLink) {
+        const textData = await getFileAsText(bill.billLink);
+        if (textData) {
+          query = textData;
+        }
+      } else {
+        query = bill.name;
+        if (bill.summary) {
+          query += ` ${ bill.summary }`;
+        }
+      }
+      const embeddedData = await embeddingService.embedData(query);
+      toPromise.push(this.model.findOneAndUpdate({ _id: bill._id }, { vector: embeddedData }));
+      logger.info({ message: `Embedding bill #${ billNumber++ } completed`, billId: bill._id });
+    }
+    await Promise.all(toPromise);
+  }
 }
 
 export default new BillsRepo();
--- src/modules/bill/bill.worker.ts
+++ src/modules/bill/bill.worker.ts
@@ -1,59 +1,58 @@
-import logger from "../../utils/logger";
-import committeeRepo from "../committee/committee.repo";
-import billRepo from "./bill.repo";
+import logger from '../../utils/logger';
+import committeeRepo from '../committee/committee.repo';
+import billRepo from './bill.repo';
 
 
 class billWorker {
+  async fetchBills(job: any) {
+    try {
+      logger.info({ message: 'Fetch bills process started', jobId: job.id });
+      await billRepo.fetchBillsFromKnesset();
+      logger.info('Fetching bills process finished');
+      return true;
+    } catch (error) {
+      logger.error('Error in fetchBills', error);
+      throw error;
+    }
+  }
 
-    async fetchBills(job:any){
-        try {
-            logger.info({message:"Fetch bills process started", jobId: job.id});
-            await billRepo.fetchBillsFromKnesset();
-            logger.info("Fetching bills process finished");
-            return true;
-        } catch (error) {
-            logger.error("Error in fetchBills", error);
-            throw error;
+
+  async updateBillsMainCategory(job: any) {
+    try {
+      logger.info({ message: 'Update bills main category process started', jobId: job.id });
+      const bills = await billRepo.find({});
+      for (const bill of bills) {
+        if (!bill.committee) {
+          continue;
         }
-    }
-        
-
-    async updateBillsMainCategory(job:any){
-        try {
-            logger.info({message:"Update bills main category process started", jobId: job.id});
-            const bills = await billRepo.find({});
-            for(const bill of bills){
-                if(!bill.committee){
-                    continue;
-                }
-                const category = committeeRepo.categoryByCommitteeId(bill.committee.toString());
-                if(!category){
-                    continue;
-                }
-                await billRepo.update({_id: bill._id}, {categories: [category]});
-            }
-            // await billRepo.updatebillsMainCategory();
-            logger.info("Update bills main category process finished");
-            return true;
-        } catch (error) {
-            logger.error("Error in updatebillsMainCategory", error);
-            throw error;
+        const category = committeeRepo.categoryByCommitteeId(bill.committee.toString());
+        if (!category) {
+          continue;
         }
+        await billRepo.update({ _id: bill._id }, { categories: [ category ] });
+      }
+      // await billRepo.updatebillsMainCategory();
+      logger.info('Update bills main category process finished');
+      return true;
+    } catch (error) {
+      logger.error('Error in updatebillsMainCategory', error);
+      throw error;
     }
+  }
 
-    async updateBillsStages(job:any){
-        logger.info({message:"Update bill stages process started", jobId: job.id});
-        await billRepo.updateBillsStages();
-        logger.info("Update bill stages process finished");
-        return true;
-    }
+  async updateBillsStages(job: any) {
+    logger.info({ message: 'Update bill stages process started', jobId: job.id });
+    await billRepo.updateBillsStages();
+    logger.info('Update bill stages process finished');
+    return true;
+  }
 
-    async updateBillDocument(job:any){
-        logger.info({message:"Update bill document process started", jobId: job.id});
-        await billRepo.updateBillsDocumentsLinks();
-        logger.info("Update bill document process finished");
-        return true;
-    }
+  async updateBillDocument(job: any) {
+    logger.info({ message: 'Update bill document process started', jobId: job.id });
+    await billRepo.updateBillsDocumentsLinks();
+    logger.info('Update bill document process finished');
+    return true;
+  }
 }
 
 export default new billWorker();
