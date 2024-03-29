bbf2e6ba102a8177ec72fba6fb7a17a62d453e10
shalevshir <shalevshir@gmail.com>
2024-03-29 11:15:29 +0300

    prepare for prod

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -5,7 +5,7 @@ import { envVars } from './envVars';
 const dbName = envVars.DB_NAME;
 const dbPass = envVars.DB_PASSWORD;
 const connectionUri = envVars.NODE_ENV === 'production' ?
-   `mongodb+srv://${ dbName }:${ dbPass }@ekklesia.xyo4j4r.mongodb.net/staging?retryWrites=true&w=majority` :
+   `mongodb+srv://${ dbName }:${ dbPass }@ekklesia.xyo4j4r.mongodb.net/EkklesiaDB?retryWrites=true&w=majority` :
   'mongodb://localhost:27018/testNew';
 export let connection: Connection;
 const connectDB = async () => {
