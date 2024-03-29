ae0a743092329480cfcd00059b884ed93bb099e9
shalevshir <shalevshir@gmail.com>
2024-03-30 00:46:14 +0300

    create queue service

---
+++ src/utils/queue.service.ts
@@ -0,0 +1,39 @@
+import Queue, { Queue as QueueType } from 'bull';
+import { envVars } from './envVars';
+
+
+class QueueService {
+  workerQueue: QueueType;
+
+  constructor() {
+    this.workerQueue = new Queue('workerQueue', envVars.REDISCLOUD_URL || 'redis://127.0.0.1:6379');
+  }
+
+  async process(name: string, callback: (job: Queue.Job, done: Queue.DoneCallback) => void) {
+    await this.workerQueue.process(name, callback);
+  }
+
+  async add(name: string, data: any) {
+    await this.workerQueue.add(name, data);
+  }
+
+  async close() {
+    await this.workerQueue.close();
+  }
+
+  async clean() {
+    await this.workerQueue.clean(0, 'completed');
+    await this.workerQueue.clean(0, 'failed');
+  }
+
+  async getJob(jobId: string) {
+    return await this.workerQueue.getJob(jobId);
+  }
+
+  async getJobs() {
+    return await this.workerQueue.getJobs([ 'active', 'completed', 'failed' ]);
+  }
+}
+
+
+export default new QueueService();
--- src/worker.ts
+++ src/worker.ts
@@ -1,34 +1,33 @@
-import Queue from 'bull';
 import queryWorker from './modules/query/query.worker';
 import { connectDB } from './utils/db';
 import logger from './utils/logger';
 import billWorker from './modules/bill/bill.worker';
 import committeeSessionWorker from './modules/committeeSession/committeeSession.worker';
-import { envVars } from './utils/envVars';
 import personWorker from './modules/person/person.worker';
 import committeeWorker from './modules/committee/committee.worker';
+import queueService from './utils/queue.service';
 
 const startWorker = async () => {
   await connectDB();
 
-  const workerQueue = new Queue('workerQueue', envVars.REDISCLOUD_URL || 'redis://127.0.0.1:6379');
   // Person
-  workerQueue.process('fetchPeople', personWorker.fetchPeople);
+  queueService.process('fetchPeople', personWorker.fetchPeople);
 
   // Committee
-  workerQueue.process('fetchCommittees', committeeWorker.fetchCommitteesFromKnessetApi);
-  // workerQueue.process('updateCommitteesMembers', committeeWorker.updateCommitteesMembers);
+  queueService.process('fetchCommittees', committeeWorker.fetchCommitteesFromKnessetApi);
+  // queueService.process('updateCommitteesMembers', committeeWorker.updateCommitteesMembers);
 
   // Query
-  workerQueue.process('fetchQueries', queryWorker.fetchQueries);
+  queueService.process('fetchQueries', queryWorker.fetchQueries);
 
   // Bill
-  workerQueue.process('fetchBills', billWorker.fetchBills);
-  workerQueue.process('updateBillsStages', billWorker.updateBillsStages);
-  workerQueue.process('updateBillDocument', billWorker.updateBillDocument);
+  queueService.process('fetchBills', billWorker.fetchBills);
+  queueService.process('updateBillsStages', billWorker.updateBillsStages);
+  queueService.process('updateBillDocument', billWorker.updateBillDocument);
 
   // CommitteeSession
-  workerQueue.process('updateCommitteeSessions', committeeSessionWorker.fetchCommitteesSessions);
+  queueService.process('updateCommitteeSessions', committeeSessionWorker.runFetchSessionsTask);
+  queueService.process('updateCommittee', committeeSessionWorker.fetchCommitteesSessions);
 
   logger.info('Worker is running...');
 };
