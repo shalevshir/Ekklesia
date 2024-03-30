293a2b82019f4b80f9b6ca7ac800d0eeaf92a9bb
shalevshir <shalevshir@gmail.com>
2024-03-30 22:42:48 +0300

    mybe now?

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,22 +1,11 @@
 import winston from 'winston';
-import { Logtail } from '@logtail/node';
-import { LogtailTransport } from '@logtail/winston';
-import { envVars } from './envVars';
 
-const logtail = new Logtail('UwXx2pbs8fhNhLK3NfGz4pgQ');
-const transports = [
+const transports =
   new winston.transports.Console({
     format: winston.format.combine(
-      winston.format.colorize(),
       winston.format.simple(),
     )
-  })
-] as winston.transport[];
-console.log(envVars.NODE_ENV);
-if (envVars.NODE_ENV === 'production') {
-  const logtailTransport = new LogtailTransport(logtail);
-  transports.push(logtailTransport);
-}
+  });
 
 const logger = winston.createLogger({
   transports
