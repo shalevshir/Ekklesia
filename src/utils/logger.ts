03ff58f21470c5354492824234374f3e514043ba
shalevshir <shalevshir@gmail.com>
2024-03-30 19:15:20 +0300

    test log level

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,16 +2,22 @@ import winston from 'winston';
 
 const logger = winston.createLogger({
   format: winston.format.json(),
+  level: 'error',
   transports: [
     new winston.transports.Console({
       format: winston.format.simple()
     })
   ],
-  levels: winston.config.syslog.levels
+  exceptionHandlers: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+      level: 'error'
+    })
+  ]
 });
 
 logger.error('This is an error message');
-logger.warn('This is a warning message');
+// logger.warn('This is a warning message');
 logger.info('This is an info message');
 
 
