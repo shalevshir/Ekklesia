c093dfd5775fcbebc22de0a4c72256b9fd3ca317
shalevshir <shalevshir@gmail.com>
2024-03-30 19:26:08 +0300

    try stderr

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,11 +1,16 @@
 import winston from 'winston';
 
 const logger = winston.createLogger({
+
+  level: 'error',
   format: winston.format.json(),
   transports: [
     new winston.transports.Console({
       format: winston.format.simple()
-    })
+    }),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+
   ]
 });
 
