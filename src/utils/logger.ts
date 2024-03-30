e3ccae92bc2db115d872120c0877c50513fdabbc
shalevshir <shalevshir@gmail.com>
2024-03-30 22:55:14 +0300

    last one

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,22 @@
-import winston from 'winston';
+import { Logtail } from '@logtail/node';
+import winston, { transport } from 'winston';
+import { envVars } from './envVars';
+import { LogtailTransport } from '@logtail/winston';
 
-const transports =
+const transports: transport[] = [];
+const consoleTransport =
   new winston.transports.Console({
     format: winston.format.combine(
-      winston.format.json(),
+      winston.format.colorize(),
+      winston.format.simple(),
     )
   });
-
+transports.push(consoleTransport);
+const logtail = new Logtail('UwXx2pbs8fhNhLK3NfGz4pgQ');
+const logtailTransport = new LogtailTransport(logtail);
+if (envVars.NODE_ENV === 'production') {
+  transports.push(logtailTransport);
+}
 const logger = winston.createLogger({
   transports
 });
