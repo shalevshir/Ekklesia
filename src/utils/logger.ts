90bd5dfbc26803dba78134c217cca3e587c165d4
shalevshir <shalevshir@gmail.com>
2024-03-30 20:13:03 +0300

    try combine format

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,17 +3,20 @@ import winston from 'winston';
 const logger = winston.createLogger({
 
   // level: 'error',
-  // format: winston.format.json(),
-  // transports: [
-  //   new winston.transports.Console({
-  //     format: winston.format.simple()
-  //   }),
-  //   new winston.transports.File({ filename: 'error.log', level: 'error' }),
-  //   new winston.transports.File({ filename: 'combined.log' })
+  format: winston.format.combine(
+    winston.format.splat(),
+    winston.format.timestamp(),
+    winston.format.json(),
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+    //   new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    //   new winston.transports.File({ filename: 'combined.log' })
 
-  // ]
+  ]
 });
-winston.error('This is an error message');
 // class Logger {
 //   logger: winston.Logger;
 //   constructor() {
@@ -41,7 +44,7 @@ winston.error('This is an error message');
 
 // logger.level = 'error';
 
-// logger.error('This is an error message');
+logger.error('This is an error message');
 // // logger.warn('This is a warning message');
 // logger.info('This is an info message');
 
