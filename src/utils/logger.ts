17eed1546763e5f0626bf68155694e7ca4ccc4df
shalevshir <shalevshir@gmail.com>
2024-03-30 19:00:54 +0300

    fix logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,8 +3,18 @@ import winston from 'winston';
 const logger = winston.createLogger({
   format: winston.format.json(),
   transports: [
-    new winston.transports.Console()
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
   ]
 });
+
+logger.error('This is an error message');
+logger.warn('This is a warning message');
+logger.info('This is an info message');
+logger.verbose('This is a verbose message');
+logger.debug('This is a debug message');
+logger.silly('This is a silly message');
+
 
 export default logger;
