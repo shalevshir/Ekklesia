26b369dec525a4b33b0f19bda52f13a08d8f138e
shalevshir <shalevshir@gmail.com>
2024-03-30 19:20:30 +0300

    fix log level

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,19 +2,14 @@ import winston from 'winston';
 
 const logger = winston.createLogger({
   format: winston.format.json(),
-  level: 'error',
   transports: [
     new winston.transports.Console({
       format: winston.format.simple()
     })
-  ],
-  exceptionHandlers: [
-    new winston.transports.Console({
-      format: winston.format.simple(),
-      level: 'error'
-    })
   ]
 });
+
+logger.level = 'error';
 
 logger.error('This is an error message');
 // logger.warn('This is a warning message');
