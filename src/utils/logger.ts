641adea0382f834904bf23deccf1d4628e205fb5
shalevshir <shalevshir@gmail.com>
2024-03-30 21:13:52 +0300

    try without format

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,20 +1,15 @@
 import winston from 'winston';
 import { Logtail } from '@logtail/node';
-import { LogtailTransport } from '@logtail/winston';
-import { envVars } from './envVars';
 
 const logtail = new Logtail('UwXx2pbs8fhNhLK3NfGz4pgQ');
 const transports = [];
-if (envVars.NODE_ENV === 'production') {
-  const logtailTransport = new LogtailTransport(logtail, {
-    format: winston.format.json()
-  });
-  transports.push(logtailTransport as any);
-} else {
-  transports.push(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
-}
+// if (envVars.NODE_ENV === 'production') {
+//   const logtailTransport = new LogtailTransport(logtail, {
+//   });
+//   transports.push(logtailTransport as any);
+// } else {
+transports.push(new winston.transports.Console());
+// }
 
 
 const logger = winston.createLogger({
