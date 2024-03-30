4074ea0a88c522f6b83f15fe737ae9cd495af1d6
shalevshir <shalevshir@gmail.com>
2024-03-30 22:39:54 +0300

    fix tyoe

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,15 +4,18 @@ import { LogtailTransport } from '@logtail/winston';
 import { envVars } from './envVars';
 
 const logtail = new Logtail('UwXx2pbs8fhNhLK3NfGz4pgQ');
-const transports = [ new winston.transports.Console({
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.simple(),
-  )
-}) ];
+const transports = [
+  new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple(),
+    )
+  })
+] as winston.transport[];
+console.log(envVars.NODE_ENV);
 if (envVars.NODE_ENV === 'production') {
   const logtailTransport = new LogtailTransport(logtail);
-  transports.push(logtailTransport as any);
+  transports.push(logtailTransport);
 }
 
 const logger = winston.createLogger({
